c4e4922832b05174f01c118b51d7aa3a60123aa9
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-05 15:37:29 -0200

    concerto de erro

--- exercicio2/exercicio2/Program.cs
+++ exercicio2/exercicio2/Program.cs
@@ -10,34 +10,33 @@ namespace exercicio2
     {
         static void Main(string[] args)
         {
-            int inicio, fim, contador, tabuada, resultado, soma=0;
+            int inicio, fim, contador, tabuada, resultado, soma = 0;
 
             Console.WriteLine("Informe o inicio");
             inicio = int.Parse(Console.ReadLine());
-            Console.WriteLine("Informe o final");
-            fim = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine("Informe o final");
+                fim = int.Parse(Console.ReadLine());
+            } while (fim < inicio);
+            
             Console.WriteLine("Informe o tabuada");
             tabuada = int.Parse(Console.ReadLine());
 
-           if(inicio < fim)
+
+            for (contador = inicio; contador <= fim; contador++)
             {
-                for (contador = inicio; contador <= fim; contador++)
-                {
-                    resultado = tabuada * contador;
-                    Console.WriteLine($"{tabuada} x {contador} = {resultado} ");
-                    soma += resultado;
-                }
+                resultado = tabuada * contador;
+                Console.WriteLine($"{tabuada} x {contador} = {resultado} ");
+                soma += resultado;
             }
-            else
-            {
-                Console.Write("Informe um inicio MENOR que o final!");
-            }
-          
+
+
 
             Console.WriteLine(soma);
             Console.ReadKey();
 
-            
+
         }
     }
 }
