2a608ac203795e7e214780814c01d7d40d0e049f
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-08 13:53:37 -0300

    exercicio 4 arrays

---
+++ array_vetores/array_vetores/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_vetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 1º jeito:   int [] x;
+                           x = new int [10];   */
+
+            // 2º jeito:  int [] x = new int [10];
+
+            int[] x = new int[5];
+            
+
+            Console.WriteLine("informe 5 valores inteiros: ");
+            for (int i = 0; i < 5  ;i++)
+            {
+                Console.Write("Informe " + (i + 1)  + "º um valor: ");
+                x[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Os valores informados são: ");
+            for (int i = 0; i < 5; i++)
+            {
+                /* if(i%2 == 0)
+                {
+                    Console.WriteLine("x[" + i + "]=" + x[i]);
+                }*/
+                Console.WriteLine("x[" + i + "]=" + x[i]);
+                //Console.WriteLine("x[" + 3 + "]=" + x[3]);
+
+            }
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ exercicio_array/exercicio_array/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            int[] x = new int[10];
+            int soma = 0;
+            int num;
+     
+            Console.WriteLine("informe 10 valores pares: ");            
+            do
+            {
+                Console.Write("Informe " + (contador + 1) + "º um valor: ");
+                num = int.Parse(Console.ReadLine());
+                if (num % 2 == 0)
+                {
+                    x[contador] = num;
+                    contador++;
+                }
+
+            } while (contador < x.Length);
+
+            Console.WriteLine("\nOs valores maiores que 10 são: ");
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] > 10)
+                {
+                    Console.WriteLine(x[i]);
+                }
+
+                soma += x[i];
+            }
+            Console.WriteLine("\nA soma de todos os numeros é de: " + soma);
+            Console.ReadKey();
+
+
+            /* RESOLUÇÃO DO PROFESSOR
+             
+             int [] vet = new int [10];
+             int soma = 0;
+
+            Console.WriteLine("Informe 10 valores pares: ");
+            for(int i=0; i<vet.Length; i++)
+            {
+                do
+                {
+                    Console.WriteLine((i + 1) + "o. valor = ");
+                    vet[i] = int.Parse(Console.ReadLine());
+
+                }while(vet[i] % 2 != 0);
+            }
+
+            Console.WriteLine("Os valores maiores do que 10 informado são: ");
+            for (int i = 0; i<vet.Length; i++) 
+            {
+                if(vet[i]>10)
+                    Console.WriteLine(vet[i]");                
+                soma += vet[i];
+            }
+            Console.WriteLine("A soma dos valores é de: " + soma)
+            Console.ReadKey();         
+             */
+
+        }
+    }
+}
