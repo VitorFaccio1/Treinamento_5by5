b6dba06ffa1b39c61b5d36f1e01088762b1afcae
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-08 15:42:33 -0300

    inverter array

---
+++ exercicio3_array/exercicio3_array/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] x = new int [10];
+            int aux;
+            int i = 0;
+
+            Console.WriteLine("Informe 10 valores: ");
+
+            for (; i < x.Length; i++)
+            {
+                Console.WriteLine("Informe o " + (i+1) + "º valor: ");
+                x[i] = int.Parse(Console.ReadLine());
+
+                for (int j = 0; j < x.Length; j++)
+                {
+
+                    if (x[i] < x[j])
+                    {
+                        aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                    }
+                }
+            }
+            for (int k = 0; k < x.Length; k++)
+            {
+                Console.Write(x[k]+"-");
+            }         
+            Console.ReadKey();
+            
+        }
+    }
+}
