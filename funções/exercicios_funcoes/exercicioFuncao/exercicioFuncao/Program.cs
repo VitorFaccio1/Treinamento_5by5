a5b2f69391fcc3c7c956f05443a9e236924dcdaa
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-09 17:32:55 -0300

    funcao

---
+++ funções/exercicios_funcoes/exercicioFuncao/exercicioFuncao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicioFuncao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o primeiro numero inteiro: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o segundo numero inteiro: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o teceiro numero inteiro: ");
+            int num3 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(Maior(num1, num2, num3));
+            Console.ReadKey();
+        }
+
+        static string Maior(int num1, int num2, int num3)
+        {
+            if (num1 > num2 && num1 > num3)
+            {
+                return ("O primeiro numero é o maior");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                return ("O segundo valor é o maior");
+            }
+            else
+            {
+                return ("O terceiro valor é o maior");
+            }
+        }
+    }
+}
---
+++ funções/função/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace função
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a primeira nota");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a segunda nota");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe a terceira nota");
+            double n3 = double.Parse(Console.ReadLine());
+            //double resp;
+
+
+            // resp = media(n1, n2, n3);
+            //Console.WriteLine("A media dos valores é de: " + resp);
+            Console.WriteLine("A media dos valores é de: " + media(n1,n2,n3));
+            Console.ReadKey();
+        }      
+
+        static double media(double n1, double n2 , double n3)
+        {
+            //double med = (n1 + n2 + n3) / 3;
+            //return (med);
+            return ((n1 + n2 + n3 )/ 3);
+        }
+      
+
+
+    }
+}
