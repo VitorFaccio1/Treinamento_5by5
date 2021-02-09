08e96f0295aa64b07d4437215fcfe382379d4a9f
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-09 17:26:06 -0300

    fibonacci_arrays

---
+++ array_vetores/exercicios_array/fibonacci/fibonacci/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor = new int[30];
+            int para;
+            vetor[0] = 0;
+            vetor[1] = 1;
+
+            do	{
+
+                Console.WriteLine("Informe um valor de parada: ");
+                para = int.Parse(Console.ReadLine());
+         	} while (para<2 || para>30);
+
+            Console.WriteLine(vetor[0]);
+            Console.WriteLine(vetor[1]);
+            for (int i = 2; i < para; i++)
+            {
+                vetor[i] = (vetor[i - 1]) + (vetor[i - 2]);         
+                Console.WriteLine(vetor[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
