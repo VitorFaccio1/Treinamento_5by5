c1e37f1596832009bd28897c01bf9a369616b1b5
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-10 13:01:16 -0300

    exercicios
    10/02

---
+++ array_vetores/exercicios_array/exercicio6_array_ordenar_localizar/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio6_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = new double[10];
+            int valor;
+            bool achou = false;
+            
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine("Informe o " + (i+1) + "º valor: ");
+                x[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                for (int j = (i+1); j < x.Length; j++)
+                {
+                    if (x[i] > x[j])
+                    {
+                        double aux = x[i];
+                        x[i] = x[j];
+                        x[j] = aux;
+                    }                 
+                }                      
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i] +"-"); //imprimindo valor ordenado
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Informe um valor para ser localizado: ");
+            valor = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == valor)
+                {
+                    Console.WriteLine("Valor encontrado é: " + x[i] + " na posição " + i);
+                    achou = true;
+                }               
+            }
+            if(!achou) // achou == false
+            {
+                Console.WriteLine("Nenhum valor foi encontrado");
+            }      
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ array_vetores/exercicios_array/exercicio7_array_inverter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio5_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] x = new double[5];
+            
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.WriteLine("Informe o " + (i+1) + "º valor: ");
+                x[i] = double.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i]+"-");
+            }
+            
+            for (int i = 0, j = (x.Length-1); i < j; i++, j--)
+            {
+                double aux = x[i]; // aux pega o valor de xI
+                x[i] = x[j];       // xi pega o valor de xj
+                x[j] = aux;        // xj pega o valor de aux(que na verdade é o x1)  
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < x.Length; i++)
+            {
+                Console.Write(x[i]+"-");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
