4a1ca062c54433a12366b7d4be0d49bf69561840
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-11 15:31:02 -0300

    11/02

---
+++ array_vetores/exercicios_array/exercicio8_inveter_arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio3_matriz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vet = new int[10];
+            int[] invertido = new int[10];
+
+            //definir valores para o vetor
+            Console.WriteLine("Informe 10 valores\n");
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.WriteLine($"informe {(i + 1)}valor: ");
+                vet[i] = int.Parse(Console.ReadLine());
+                
+            }
+
+            //mostrar vetor
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write(vet[i]+"-");
+            }
+
+            //inverter valores
+            for (int i = 0, j = (vet.Length -1); i < vet.Length; i++, j--)
+            {                
+                invertido[j] = vet[i];
+            }
+
+            Console.WriteLine();
+            //mostrar resultado invertido
+            for (int i = 0; i < vet.Length; i++)
+            {
+                Console.Write(invertido[i] + "-");
+            }
+
+
+            Console.ReadKey();
+            
+        }
+    }
+}
