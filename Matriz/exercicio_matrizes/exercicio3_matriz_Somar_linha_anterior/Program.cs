51e88c5dcdaa0883b766e3f248ebaf5dfbe9945b
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-11 19:00:34 -0300

    11/02

---
+++ Matriz/exercicio_matrizes/exercicio3_matriz_Somar_linha_anterior/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace exercicio3_matriz_verRepetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[10, 5];
+
+            //PASSAR VALORES PARA MATRIZ
+            
+            Leitura(matriz);
+
+            Calcular(matriz);
+
+            Imprimir(matriz);
+
+            Soma(matriz);         
+
+            Console.ReadKey();
+
+        }
+        
+        static void Leitura(int [,] mat)
+        {
+            Console.WriteLine("Informe o valores: ");
+            for (int j = 0; j < mat.GetLength(1); j++)
+            {
+                Console.WriteLine("informe valor da " + (j + 1) + "º coluna ");
+                mat[0, j] = int.Parse(Console.ReadLine());
+            }
+
+            return;
+        }
+
+        static void Calcular(int[,] mat)
+        {
+            for (int j = 0; j < mat.GetLength(1); j++)
+            {
+                for (int i = 1; i < mat.GetLength(0); i++)
+                {
+                    mat[i, j] = mat[0, j] * (i + 1);
+                }
+            }
+            return;
+        }
+
+        static void Imprimir(int [,] mat)
+        {
+            for (int i = 0; i < mat.GetLength(0); i++)
+            {
+                for (int x = 0; x < mat.GetLength(1); x++)
+                {
+                    Console.Write(mat[i, x] + "-");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Soma(int[,]mat)
+        {
+            int soma = 0;
+            for (int i = 0; i < mat.GetLength(1); i++)
+            {
+                soma += mat[9, i];
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
