4af2b868ee131fe464edb0242957b59c5535416f
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-11 14:17:46 -0300

    11/02

---
+++ Matriz/exercicio_matrizes/exercicio1_matrizes_localizar_valor/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio2_matrizes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matriz = new int[10, 2];
+            int valor;
+            bool achou = false;
+
+            //PASSAR VALORES PARA MATRIZ
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Informe o valor da " +(i+1) +"º linha");
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.WriteLine((j+1)+"º coluna ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            //INFORMAR VALOR DE BUSCA
+            Console.WriteLine("Informe o valor que voce quer localizar: ");
+            valor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            //AJEITAR MATRIZ 
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    Console.Write(matriz[i,k] + "\t");                    
+                   
+                }
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("");
+            //MOSTRAR RESULTADO DA POSIÇÃO DOS VALORES
+            for (int i = 0; i < 10; i++)
+            {
+                for (int k = 0; k < 2; k++)
+                {
+                    if (valor == matriz[i, k])
+                    { 
+                        Console.WriteLine("O valor "+valor+" foi encontrados na posição [" +(i+1)+ ","+ (k+1) +"]" );
+                        achou = true;
+                    }
+                }
+            }
+
+            if (!achou)
+            {
+                Console.WriteLine("O valor nao existe dentro da matriz");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ funções/funcao_exemplo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcao_teoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine($"Antes de chamar a função trocar\n a= {a} b={b}");
+            trocar_valores(ref a,ref b);
+            Console.WriteLine($"DENTRO DO MAIN\nValoers trocados de a e b \n a= {a} b= {b}");
+            Console.ReadKey();
+        }
+
+        static void trocar_valores(ref int a ,ref int b)
+        {
+            int aux;
+            aux = a;
+            a = b;
+            b = aux;
+            Console.WriteLine($"DENTRO DA FUNÇÃO\nValoers trocados de a e b \n a= {a} b= {b}");
+            return;
+        }
+    }
+}
