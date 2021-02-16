35c5889c9ef0cab77b21b5f96bffd678d859236f
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-16 11:58:06 -0300

    POO

---
+++ POO/Exemplo_Classe1/Pessoa.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Classe1
+{
+    class Pessoa
+    {
+        #region exemplo geral de atributo
+        //private int id;
+        public int id;
+
+
+        public int GetId()
+        {
+            return id;
+        }
+
+        public void SetId(int id)
+        {
+            this.id = id;
+        }
+        #endregion
+
+        #region Exemplo 2 - c#
+        private string nome;
+
+        public string Nome
+        {
+            get
+            {
+                return this.nome;
+            }
+
+            set
+            {
+                this.nome = value;
+            }
+        }
+        #endregion
+
+        #region Exemplo 3 - propriedade c#
+        public int Idade { get; set; }
+        #endregion
+        public override string ToString()
+        {
+            return ($"Id: {this.id}, Nome: {nome} e idade: {Idade} ");
+        }
+
+        #region Metodos costrutores tradicionais
+        /* METODOS CONTRUTORES TRADICIONAIS
+        public Pessoa()
+        {
+            id = 0;
+            Nome = "zé ninguem";
+            Idade = 0;
+        }
+
+        public Pessoa(int id, string nome, int idade)
+        {
+            this.id = id;
+            Nome = nome;
+            Idade = idade;
+        }*/
+
+        #endregion
+
+
+
+        public bool Maior()
+        {
+            if (Idade >= 18)            
+                return true;
+            return false;
+            
+        }
+    }
+}
---
+++ POO/Exemplo_Classe1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Classe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p = new Pessoa();
+            Pessoa p2 = new Pessoa
+                {
+                id = 1,
+                Nome = "Guilherme",
+                Idade= 12
+                };
+
+            Console.WriteLine(p);
+            Console.WriteLine(p.ToString());
+            
+            Console.WriteLine();
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine();
+
+            Console.WriteLine("Apos instanciar o objeto ID: {0}", p.GetId());
+            Console.WriteLine("Apos instanciar o objeto Nome: {0}", p.Nome);
+            Console.WriteLine("Apos instanciar o objeto Idade: {0}", p.Idade);
+
+            Console.WriteLine("-------APOS CHAMAR O METODO-------");
+
+            p.Nome = "Vitor";
+            p.SetId(10);
+            p.Idade = 18;
+
+            Console.WriteLine("Apos chamar o metodo SetId: {0}", p.GetId());
+            Console.WriteLine("Apos chamar o metodo Nome: {0}", p.Nome);
+            Console.WriteLine("Apos chamar o metodo idade: {0}", p.Idade);
+
+            Console.WriteLine();
+
+            Console.WriteLine(p.ToString());
+            if (p.Maior())
+            {
+                Console.WriteLine($"{p.Nome} é maior de idade");
+            }
+            else
+            {
+                Console.WriteLine($"{p.Nome} é menor de idade");
+            }
+            Console.ReadKey();
+        }
+    }
+}
