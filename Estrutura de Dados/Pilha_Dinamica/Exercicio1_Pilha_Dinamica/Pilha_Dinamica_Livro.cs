9f909941e4d24058d1aad359445fd39999c1c0dc
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-17 17:42:34 -0300

    exercicio pilha

---
+++ Estrutura de Dados/Pilha_Dinamica/Exercicio1_Pilha_Dinamica/Pilha_Dinamica_Livro.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha_Dinamica_Simplesmente_Encadeada
+{
+    class Pilha_Dinamica_Livro
+    {
+        public Livro Topo { get; set; }
+        //pop voce remove e push voce adciona!
+
+        public void Push(Livro aux)
+        {
+            aux.anterior = Topo;
+            Topo = aux;
+            Console.WriteLine("\nElemento inserido com sucesso!\n");
+            return;
+        }
+
+        private bool Vazia()
+        {
+            if (Topo == null)
+                return true;
+            return false;
+        }     
+
+        public void Imprimir()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("\nA pilha está vazia! Impossivel imprimir!\n");
+            }
+            else
+            {
+                Livro aux = Topo;
+                Console.WriteLine("\nOs elementos na pilha são: ");
+                do
+                {
+                    Console.WriteLine(aux.ToString());
+                    aux = aux.anterior;
+                } while (aux != null);
+                Console.WriteLine(">>>FIM<<<\n");
+            }
+        }
+
+        public void Pop()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("\nImpossivel Remover! Pilha vazia!\n");
+            }
+            else
+            {
+                Topo = Topo.anterior;
+                Console.WriteLine("Elemento removido!");
+            }
+        }
+
+        public void Busca(Livro[] livros, int contador)
+        {
+            string titulo;
+            Console.WriteLine("Qual titulo você quer buscar: ");
+            titulo = Console.ReadLine().Trim(' ').ToLower();
+
+            for (int i = 0; i < contador; i++)
+            {
+                if(livros[i].Titulo == titulo)
+                {
+                    Console.WriteLine("\nTem esse livro!\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nNao tem esse livro!\n");
+                }
+            }         
+            return;
+        }
+
+
+    }
+}
---
+++ Estrutura de Dados/Pilha_Dinamica/Exercicio1_Pilha_Dinamica/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha_Dinamica_Simplesmente_Encadeada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Livro[] livros = new Livro[5];
+            Pilha_Dinamica_Livro minha_pilha = new Pilha_Dinamica_Livro();
+            string escolha;
+
+            int contador = 0;
+            do
+            {
+                Console.WriteLine("Informe o que você quer fazer" +
+                                  "\n1- Inserir livro " +
+                                  "\n2- Remover Livro" +
+                                  "\n3- Imprimir pilha" +
+                                  "\n4- Quantidade de elementos na pilha" +
+                                  "\n5- Busca de livro na pilha por titulo" +
+                                  "\n0- Sair ");
+
+                escolha = Console.ReadLine();               
+
+                switch (escolha)
+                {
+                    case "1":
+                        {
+                            if (contador < livros.Length)
+                            {
+                                Console.WriteLine("Insira os dados do livro que você quer adcionar");
+                                livros[contador] = Inserir();
+                                minha_pilha.Push(livros[contador]);
+                                contador++;                                                              
+                            }
+                            else
+                            {
+                                Console.WriteLine("Voce atingiu o numero maximo de livros que podem ser cadastrados!");
+                            }                              
+                            break;
+
+                        }
+                    case "2":
+                        {
+                            minha_pilha.Pop();
+                            contador--;
+                            break;
+                        }
+                    case "3":
+                        {
+                            minha_pilha.Imprimir();
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("\nA quantidade de livros é: "+ contador + "\n"); 
+                            break;
+                        }
+                    case "5":
+                        {
+                            if(contador == 0)
+                            {
+                                Console.WriteLine("\nNao existe livros cadastrados\n");
+                            }
+                            else
+                            {
+                                minha_pilha.Busca(livros, contador);
+                            }
+                            break;
+                        }
+                }
+
+
+
+            } while (escolha != "0");
+            
+            Console.ReadKey();
+        }
+
+        static public Livro Inserir()
+        {
+
+            int isbn;
+            string titulo, autor;
+
+
+            Console.WriteLine("Informe o titulo do livro: "); titulo = Console.ReadLine();
+            Console.WriteLine("Informe o autor do livro: "); autor = Console.ReadLine();
+            Console.WriteLine("Informe o isbn do livro: "); isbn = int.Parse(Console.ReadLine());
+
+
+            Livro book = new Livro
+            {
+                Titulo = titulo,
+                Autor = autor,
+                Isbn = isbn,
+            };
+
+            return book;
+        }
+
+
+    }
+}
