6b6855b3abf13fa5fc5b880e64eefce4523d11bf
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-17 11:08:10 -0300

    Create Funcionario.cs

---
+++ POO/Exemplo_Classe1/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Classe1
+{
+    class Funcionario : Pessoa
+    {
+        #region Propriedades
+        public int QtadFilhos { get; set; }
+        public string Id { get; set; }
+        #endregion
+
+        public override string ToString()
+        {
+            return base.ToString(); 
+        }
+    }
+}
