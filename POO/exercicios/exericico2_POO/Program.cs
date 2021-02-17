1ad237e2742a64778ea010da19faad8cc09b312b
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-16 22:25:53 -0300

    ajuste

--- POO/exercicios/exericico2_POO/Program.cs
+++ POO/exercicios/exericico2_POO/Program.cs
@@ -9,7 +9,7 @@ namespace exericico2_POO
     class Program
     {
         static void Main(string[] args)
-        {   
+        {
             string escolha;
             cliente c = new cliente();
             c.account = new conta();
@@ -26,68 +26,69 @@ namespace exericico2_POO
             };
 
 
+
             do
             {
-                do
+                Console.WriteLine("\nMenu:\n" +
+                          "1)Mostra os dados necessarios para conta ser criada\n" +
+                          "2)Deposito\n" +
+                          "3)Saque\n" +
+                          "4)Transferencia\n" +
+                          "5)Imprimia o saldo\n" +
+                          "6)Sair do programa");
+                escolha = Console.ReadLine();
+
+                if (c.CPF == 0 && escolha != "1" && escolha != "6")
                 {
-                    Console.WriteLine("\nMenu:\n" +
-                              "1)Mostra os dados necessarios para conta ser criada\n" +
-                              "2)Deposito\n" +
-                              "3)Saque\n" +
-                              "4)Transferencia\n" +
-                              "5)Imprimia o saldo\n" +
-                              "6)Sair do programa");
-                    escolha = Console.ReadLine();
-
-                    if (escolha == "2" || escolha == "3" || escolha == "4" || escolha == "5")
-                    {
-                        Console.WriteLine("\nPrimeiro Crie Sua conta para depois fazer as outras coisa");
-                    }
-                } while (escolha != "1" && escolha != "6");
-               
-
-                switch (escolha)
-                {
-                    case "1":
-                        {
-                            c.Inserir();
-                            break;
-                        }
-                    case "2":
-                        {
-                            double valor;
-
-                            Console.WriteLine("----FAZER DEPÓSITO----");
-                            Console.WriteLine("Qual valor que você quer depositar: ");
-                            valor = double.Parse(Console.ReadLine());
-                            c.account.Deposito(valor);
-                            break;
-                        }
-                    case "3":
-                        {
-                            double valor;
-
-                            Console.WriteLine("----FAZER SAQUE----");
-                            Console.WriteLine("Qual valor que você quer sacar: ");
-                            valor = long.Parse(Console.ReadLine());
-                            c.account.Saque(valor);
-                            break;
-                        }
-                    case "4":
-                        {
-                            c.account.Transferencia(joao);
-                            break;
-                        }
-                    case "5":
-                        {
-                            Console.WriteLine("Seu saldo é de: " + c.account.saldo);
-                            break;
-                        }
-                    case "6":
-                        {
-                            break;
-                        }
+                    Console.WriteLine("\nPrimeiro Crie Sua conta para depois fazer as outras coisa");
                 }
+                else
+                {
+                    switch (escolha)
+                    {
+                        case "1":
+                            {
+                                c.Inserir();
+                                break;
+                            }
+                        case "2":
+                            {
+                                double valor;
+
+                                Console.WriteLine("----FAZER DEPÓSITO----");
+                                Console.WriteLine("Qual valor que você quer depositar: ");
+                                valor = double.Parse(Console.ReadLine());
+                                c.account.Deposito(valor);
+                                break;
+                            }
+                        case "3":
+                            {
+                                double valor;
+
+                                Console.WriteLine("----FAZER SAQUE----");
+                                Console.WriteLine("Qual valor que você quer sacar: ");
+                                valor = long.Parse(Console.ReadLine());
+                                c.account.Saque(valor);
+                                break;
+                            }
+                        case "4":
+                            {
+                                c.account.Transferencia(joao);
+                                break;
+                            }
+                        case "5":
+                            {
+                                Console.WriteLine("Seu saldo é de: " + c.account.saldo);
+                                break;
+                            }
+                        case "6":
+                            {
+                                break;
+                            }
+                    }
+                }
+
+
             } while (escolha != "6");
 
 
