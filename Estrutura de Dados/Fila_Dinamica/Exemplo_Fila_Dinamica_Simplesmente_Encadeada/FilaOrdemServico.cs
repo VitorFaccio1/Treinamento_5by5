ec250aa3bd3d2173d2e464d88774f362c125bf41
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-18 14:24:01 -0300

    Fila Encadeada

---
+++ Estrutura de Dados/Fila_Dinamica/Exemplo_Fila_Dinamica_Simplesmente_Encadeada/FilaOrdemServico.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Fila_Dinamica_Simplesmente_Encadeada
+{
+    class FilaOrdemServico
+    {
+        public OrdemServico Head { get; set; }
+        public OrdemServico Tail { get; set; }
+
+        public bool Vazia()
+        {
+            if (Head == null && Tail == null)
+            {
+                Console.WriteLine("\nA fila esta vazia\n");
+                return true;
+            }
+            return false;
+        }
+
+        public void Push(OrdemServico aux)
+        {
+            if (Vazia())
+            {
+                Head = aux;
+                Tail = aux;
+            }
+            else
+            {
+                Tail.Proximo = aux; //Encadeamento da nova ordem de serviço
+                Tail = aux; //atualizo o tail               
+            }
+            Console.WriteLine("\nElemento inserido com sucesso!!!\n");
+        }
+
+        public void Imprimir()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("\nA fila esta vazia!!!\n");
+            }
+            else
+            {
+               
+                OrdemServico aux = Head;
+                do
+                {
+                    Console.WriteLine(aux.ToString());
+                    aux = aux.Proximo;
+                } while (aux != null);
+                Console.WriteLine("----------------------FIM----------------------");                
+            }
+        }
+
+        public void Pop()
+        {
+            if (Vazia())
+            {
+                Console.WriteLine("A fila está vazia!!!\n");
+            }
+            else
+            {
+                Head = Head.Proximo;
+                if (Head == null)
+                {
+                    Tail = null;                   
+                }
+                Console.WriteLine("\nElemento apagado com sucesso!!!\n");
+            }
+        }
+
+
+
+    }
+}
---
+++ Estrutura de Dados/Fila_Dinamica/Exemplo_Fila_Dinamica_Simplesmente_Encadeada/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_Fila_Dinamica_Simplesmente_Encadeada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FilaOrdemServico filaos = new FilaOrdemServico
+            {
+                Head = null,
+                Tail = null
+            };
+
+            OrdemServico os = new OrdemServico
+            {
+                Numero = 1,
+                Tipo = "Usinagem",
+                Descricao = "Fazer 100 unidades de pedaleira de moto",
+                //Data_Criacao = DateTime.Parse("20/02/2020"), DATA QUE VOCE QUISER
+                Data_Criacao = DateTime.Now, //DATA DO SISTEMA
+                Prazo = 10,
+                Proximo = null
+
+            };
+            filaos.Push(os);
+            //Console.WriteLine(os.ToString());
+
+            os = new OrdemServico
+            {
+                Numero = 2,
+                Tipo = "Pintura",
+                Descricao = "Pintar 20 quadros de BMW1200",
+                //Data_Criacao = DateTime.Parse("20/02/2020"), DATA QUE VOCE QUISER
+                Data_Criacao = DateTime.Now, //DATA DO SISTEMA
+                Prazo = 15,
+                Proximo = null
+            };
+            filaos.Push(os);            
+
+            os = new OrdemServico
+            {
+                Numero = 3,
+                Tipo = "Montar moto",
+                Descricao = "Montar 10 unidades de biz 150",
+                //Data_Criacao = DateTime.Parse("20/02/2020"), DATA QUE VOCE QUISER
+                Data_Criacao = DateTime.Now, //DATA DO SISTEMA
+                Prazo = 25,
+                Proximo = null
+            };
+            filaos.Push(os);
+
+            filaos.Pop();
+            filaos.Pop();
+
+
+            filaos.Imprimir();
+
+            Console.ReadKey();
+        }
+
+       
+    }
+}
