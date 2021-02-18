5665c6fc9dde14ab962f2346ace9b22b4ff8af5d
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-18 10:30:53 -0300

    agora é a versao final kkkk

--- Estrutura de Dados/Pilha_Dinamica/Exercicio1_Pilha_Dinamica/Program.cs
+++ Estrutura de Dados/Pilha_Dinamica/Exercicio1_Pilha_Dinamica/Program.cs
@@ -10,75 +10,7 @@ namespace Pilha_Dinamica_Simplesmente_Encadeada
     {
         static void Main(string[] args)
         {
-            Livro[] livros = new Livro[5];
-            Pilha_Dinamica_Livro minha_pilha = new Pilha_Dinamica_Livro();
-            string escolha;
-
-            int contador = 0;
-            do
-            {
-                Console.WriteLine("Informe o que você quer fazer" +
-                                  "\n1- Inserir livro " +
-                                  "\n2- Remover Livro" +
-                                  "\n3- Imprimir pilha" +
-                                  "\n4- Quantidade de elementos na pilha" +
-                                  "\n5- Busca de livro na pilha por titulo" +
-                                  "\n0- Sair ");
-
-                escolha = Console.ReadLine();               
-
-                switch (escolha)
-                {
-                    case "1":
-                        {
-                            if (contador < livros.Length)
-                            {
-                                Console.WriteLine("Insira os dados do livro que você quer adcionar");
-                                livros[contador] = Inserir();
-                                minha_pilha.Push(livros[contador]);
-                                contador++;                                                              
-                            }
-                            else
-                            {
-                                Console.WriteLine("Voce atingiu o numero maximo de livros que podem ser cadastrados!");
-                            }                              
-                            break;
-
-                        }
-                    case "2":
-                        {
-                            minha_pilha.Pop();
-                            contador--;
-                            break;
-                        }
-                    case "3":
-                        {
-                            minha_pilha.Imprimir();
-                            break;
-                        }
-                    case "4":
-                        {
-                            Console.WriteLine("\nA quantidade de livros é: "+ contador + "\n"); 
-                            break;
-                        }
-                    case "5":
-                        {
-                            if(contador == 0)
-                            {
-                                Console.WriteLine("\nNao existe livros cadastrados\n");
-                            }
-                            else
-                            {
-                                minha_pilha.Busca(livros, contador);
-                            }
-                            break;
-                        }
-                }
-
-
-
-            } while (escolha != "0");
-            
+            Menu();            
             Console.ReadKey();
         }
 
@@ -102,6 +34,79 @@ namespace Pilha_Dinamica_Simplesmente_Encadeada
             };
 
             return book;
+        }
+
+        static public void Menu()
+        {
+            Pilha_Dinamica_Livro minha_pilha = new Pilha_Dinamica_Livro();
+            Livro[] livros = new Livro[5];
+
+            string escolha;
+
+            int contador = 0;
+            do
+            {
+                Console.WriteLine("Informe o que você quer fazer" +
+                                  "\n1- Inserir livro " +
+                                  "\n2- Remover Livro" +
+                                  "\n3- Imprimir pilha" +
+                                  "\n4- Quantidade de elementos na pilha" +
+                                  "\n5- Busca de livro na pilha por titulo" +
+                                  "\n0- Sair ");
+
+                escolha = Console.ReadLine();
+
+                switch (escolha)
+                {
+                    case "1":
+                        {
+                            if (contador < livros.Length)
+                            {
+                                Console.WriteLine("Insira os dados do livro que você quer adcionar");
+                                livros[contador] = Inserir();
+                                minha_pilha.Push(livros[contador]);
+                                contador++;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Voce atingiu o numero maximo de livros que podem ser cadastrados!");
+                            }
+                            break;
+
+                        }
+                    case "2":
+                        {
+                            minha_pilha.Pop();
+                            contador--;
+                            break;
+                        }
+                    case "3":
+                        {
+                            minha_pilha.Imprimir();
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("\nA quantidade de livros é: " + contador + "\n");
+                            break;
+                        }
+                    case "5":
+                        {
+                            if (contador == 0)
+                            {
+                                Console.WriteLine("\nNao existe livros cadastrados\n");
+                            }
+                            else
+                            {
+                                minha_pilha.Busca(livros, contador);
+                            }
+                            break;
+                        }
+                }
+
+
+
+            } while (escolha != "0");
         }
 
 
