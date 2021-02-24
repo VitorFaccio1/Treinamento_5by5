9c83bcdae9feadd195c94f0fe6f1652bf755dc27
VitorFaccio1 <53455640+VitorFaccio1@users.noreply.github.com>
2021-02-24 12:00:53 -0300

    Lista nutella com classes

---
+++ Estrutura de Dados/Lista_Dinamica/Exemplo2_Lista/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo2_Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pessoa> listaPessoa = new List<Pessoa>();
+            string nome;
+            int idade;
+            Console.WriteLine("Capacidade: " + listaPessoa.Capacity);
+            do
+            {
+                Console.WriteLine("Digite um nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("Digite a idade: ");
+                idade = int.Parse(Console.ReadLine());
+                if(nome != "")
+                {
+                    listaPessoa.Add(new Pessoa { Nome = nome, Idade = idade });
+                }
+            } while (nome != "");
+
+
+            Console.WriteLine("A lista tem " + listaPessoa.Count + " elementos");
+
+            listaPessoa.ForEach(i => Console.WriteLine(i.ToString()));
+
+            Console.WriteLine("\nA lista ordenada por nome: ");
+            listaPessoa = listaPessoa.OrderBy(lp => lp.Nome).ToList();
+            listaPessoa.ForEach(i => Console.WriteLine(i.ToString()));
+
+            Console.WriteLine("\nA lista ordenada por idade: ");
+            listaPessoa = listaPessoa.OrderBy(lp => lp.Idade).ToList();
+            listaPessoa.ForEach(i => Console.WriteLine(i.ToString()));
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
